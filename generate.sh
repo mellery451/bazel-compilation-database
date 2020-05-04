b65cf11166061cb2f5f7c3a4bf7f44ad0ddaa21f
Mike Ellery <mellery451@gmail.com>
2020-05-04 08:33:45 -0700

    WIP changes for dazel/dw-sdk

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Copyright 2017 GRAIL, Inc.
 #
@@ -21,8 +21,10 @@
 # https://github.com/google/kythe/blob/master/tools/cpp/generate_compilation_database.sh
 
 set -e
+#set -x
 
 source_dir=0
+b_cmd="bazel"
 
 usage() {
   printf "usage: %s flags\nwhere flags can be:\n" "${BASH_SOURCE[0]}"
@@ -30,13 +32,15 @@ usage() {
   printf "\n"
 }
 
-while getopts "sh" opt; do
+while getopts "sdh" opt; do
   case "${opt}" in
     "s") source_dir=1 ;;
+    "d") b_cmd="dazel" ;;
     "h") usage; exit 0;;
     *) >&2 echo "invalid option ${opt}"; exit 1;;
   esac
 done
+shift $((OPTIND -1))
 
 # This function is copied from https://source.bazel.build/bazel/+/master:scripts/packages/bazel.sh.
 # `readlink -f` that works on OSX too.
@@ -83,16 +87,20 @@ readonly ASPECTS_DIR="$(dirname "$(get_realpath "${BASH_SOURCE[0]}")")"
 readonly OUTPUT_GROUPS="compdb_files"
 readonly BAZEL="${BAZEL_COMPDB_BAZEL_PATH:-bazel}"
 
-readonly WORKSPACE="$("$BAZEL" info workspace)"
-readonly EXEC_ROOT="$("$BAZEL" info execution_root)"
+readonly WORKSPACE="$(${b_cmd} info workspace 2>&1 | tail -1)"
+readonly EXEC_ROOT="$(${b_cmd} info execution_root 2>&1 | tail -1)"
+readonly BAZEL_BIN="$(${b_cmd} info bazel-bin 2>&1 | tail -1)"
+readonly BAZEL_BIN_TRIM="$(echo ${BAZEL_BIN} | sed -E "s|$EXEC_ROOT/||")"
+echo "BAZEL BIN:  $BAZEL_BIN_TRIM"
 readonly COMPDB_FILE="${WORKSPACE}/compile_commands.json"
 
-readonly QUERY_CMD=(
-  "$BAZEL" query
-    --noshow_progress
-    --noshow_loading_progress
-    'kind("cc_(library|binary|test|inc_library|proto_library)", //...) union kind("objc_(library|binary|test)", //...)'
-)
+TARGETS=()
+while read -r target ; do
+    TARGETS+=(${target})
+done < <( ${b_cmd} \
+    query --noshow_progress --noshow_loading_progress \
+    'kind("cc_(library|binary|test|inc_library|proto_library)", //...) union kind("objc_(library|binary|test)", //...)' 2>&1)
+echo "NUM ELEMS: ${#TARGETS[*]}"
 
 # Clean any previously generated files.
 if [[ -e "${EXEC_ROOT}" ]]; then
@@ -100,18 +108,23 @@ if [[ -e "${EXEC_ROOT}" ]]; then
 fi
 
 # shellcheck disable=SC2046
-"$BAZEL" build \
+${b_cmd} build \
   "--override_repository=bazel_compdb=${ASPECTS_DIR}" \
   "--aspects=@bazel_compdb//:aspects.bzl%compilation_database_aspect" \
   "--noshow_progress" \
   "--noshow_loading_progress" \
   "--output_groups=${OUTPUT_GROUPS}" \
-  "$@" \
-  $("${QUERY_CMD[@]}") > /dev/null
+  "$@" > /dev/null
+#  "$@" "${TARGETS[@]}" > /dev/null
 
 echo "[" > "${COMPDB_FILE}"
-find "${EXEC_ROOT}" -name '*.compile_commands.json' -exec bash -c 'cat "$1" && echo ,' _ {} \; \
-  >> "${COMPDB_FILE}"
+while read -r found ; do
+    size=$(cat $found | wc -l)
+    if (( ${size} > 0 )) ; then
+        cat "$found" >> "${COMPDB_FILE}"
+        echo "," >> "${COMPDB_FILE}"
+    fi
+done < <(find "${EXEC_ROOT}" -name '*.compile_commands.json')
 echo "]" >> "${COMPDB_FILE}"
 
 # Remove the last occurence of a comma from the output file.
@@ -129,6 +142,102 @@ else
   ln -f -s "${WORKSPACE}/${COMPDB_FILE}" "${EXEC_ROOT}/"
 fi
 sed -i.bak -e "s|-isysroot __BAZEL_XCODE_SDKROOT__||" "${COMPDB_FILE}"  # Replace -isysroot __BAZEL_XCODE_SDKROOT__ marker
+
+sed -i.bak -e "s|-isysroot __BAZEL_XCODE_SDKROOT__||" "${COMPDB_FILE}"  # Replace -isysroot __BAZEL_XCODE_SDKROOT__ marker
+
+if [[ $b_cmd == "dazel" ]] ; then
+  sed -i.bak -E -e "s|bazel-out/.+/gcc_nvcc_wrapper|g++|g" "${COMPDB_FILE}"  # Replace gcc_nvcc with compiler
+  TO_ELIM=(-fno-canonical-system-headers
+           -Wno-free-nonheap-object
+           -Wfree-nonheap-object
+           -Wno-unused-but-set-parameter
+           -Wunused-but-set-parameter
+           -Wno-maybe-uninitialized
+           -Wmaybe-uninitialized
+           "-nvcc_options [^[:space:]]+"
+           "${BAZEL_BIN_TRIM}/")
+  for elim in  "${TO_ELIM[@]}" ; do
+    sed -i.bak -E -e "s|$elim||g" "${COMPDB_FILE}"
+  done
+
+  # extract a comprehensive list of include paths
+  # in the DB and then munge the ones that are invalid
+  while read -r inc ; do
+    inc=$(echo "${inc}" | sed -E -e "s|^[[:space:]]+||" | sed -E -e "s|[[:space:]]+$||")
+    newinc=${inc}
+    newinc=$(echo "${newinc}" | sed -E -e "s|^bazel-out/k8-[^/]+/bin/||")
+    if echo "${newinc}" | grep -q -E "/_virtual_includes/" ; then
+      newinc=$(echo "${newinc}" | sed -E -e "s|/_virtual_includes/.+||")
+      if [[ -d "${newinc}/include" ]]; then
+        moarinc="${newinc}/include"
+        if [[ -d "${newinc}/src" ]]; then
+          moarinc+=" -isystem ${newinc}/src"
+        fi
+        newinc="${moarinc}"
+      elif [[ -d "${newinc}/src" ]]; then
+        newinc+="/src"
+      fi
+    fi
+
+    # custom hacks for some "installed" 3rdparty
+    if echo "${newinc}" | grep -q "/gmock/install/" ; then
+      newinc=$(echo "${newinc}" | sed -E -e "s|/gmock/install/|/googlemock/|")
+    fi
+    if echo "${newinc}" | grep -q "/gtest/install/" ; then
+      newinc=$(echo "${newinc}" | sed -E -e "s|/gtest/install/|/googletest/|")
+    fi
+    if echo "${newinc}" | grep -q "/ion" ; then
+        newinc="3rdparty/src/ion/pkg/ionc/inc -isystem 3rdparty/src/ion/pkg/ionc"
+    fi
+    if [[ "${newinc}" == "3rdparty/src/physfs/physfs/install/include" ]]; then
+        newinc="3rdparty/src/physfs/include"
+    fi
+    if [[ "${newinc}" == "3rdparty/src/xsens/xsens/install/include" ]]; then
+        newinc="3rdparty/src/xsens/include"
+    fi
+    #echo -e "   >>> turned [$inc]\\n    >>> into [$newinc]"
+    if [[ "$newinc" != "$inc" ]]; then
+      sed -i.bak -E -e "s|[[:space:]=]+${inc}[[:space:]=]+| ${newinc} |g" "${COMPDB_FILE}"
+    fi
+  done < <( \
+    sed -E -e 's/(-I|-isystem|-iquote)[[:space:]=]+([^[:space:]]+)/\n\1 \2\n/g' "${COMPDB_FILE}" \
+    | grep -E '(-I|-isystem|-iquote) ' \
+    | sed -E -e 's/(-I|-isystem|-iquote) (.+)/\2/g' \
+    | sort | uniq)
+  ## ^^^ this matching has to be revisited if we have any paths with spaces
+
+  while read -r inc ; do
+    if [[ ! -d $inc ]]; then
+      echo "WARN: might need a manual substition for $inc (which does not exist)"
+    fi
+  done < <( \
+    sed -E -e 's/(-I|-isystem|-iquote)[[:space:]=]+([^[:space:]]+)/\n\1 \2\n/g' "${COMPDB_FILE}" \
+    | grep -E '(-I|-isystem|-iquote) ' \
+    | sed -E -e 's/(-I|-isystem|-iquote) (.+)/\2/g' \
+    | sort | uniq)
+
+  gendir="$PWD/build/_gen"
+  add_includes=""
+  #  while read -r inc; do
+  #      add_includes+=" -isystem $inc"
+  #  done < <(find 3rdparty/src 3rdparty/shared -name include)
+  add_includes+=" -I src"
+  add_includes+=" -isystem /usr/local/cuda/include"
+  add_includes+=" -isystem $gendir"
+  while read -r inc ; do
+    add_includes+=" -I $inc"
+  done < <(ls -d src/dwshared/*/)
+  #  sed -i.bak -E -e "s|-isystem 3rdparty/[^[:space:]]+/install/[^[:space:]]+ ||g" "${COMPDB_FILE}"
+  sed -i.bak -E -e "s|(.*) -isystem |\\1 $add_includes -isystem |" "${COMPDB_FILE}"
+
+  # TODO -- these all need to come from the actual build/config (generated headers)
+  mkdir -p "$gendir/dw/core"
+  echo "#define DW_RUNTIME_CHECKS() 1" > $gendir/dw/core/ConfigChecks.h
+  echo -e "#ifndef DW_CORE_CONFIG_H__\\n#define LINUX\\n#define DW_USE_NVMEDIA_X86\\n#define DW_USE_NVSIPL\\n#define DW_USE_OPENDDS\\n#endif\\n" > $gendir/dw/core/Config.h
+  echo -e "#ifndef DW_CORE_CONFIG3RDPARTY_HPP__\\n#define DW_USE_OPENCV\\n#define DW_USE_HERE\\n#define TRT_VERSION_DECIMAL 6000011\\n#define NVDLA_VERSION_DECIMAL 10400\\n#define NVMEDIA_VERSION_DECIMAL 14\\n#endif\\n" > $gendir/dw/core/Config3rdParty.hpp
+  echo -e "#ifndef DW_CORE_VERSIONCURRENT_H_\\n#include <dw/core/Version.h>\\n#define DW_VERSION dwVersion{3,0,-1,\"nil\",\"-exp\"}\\n#endif\\n" > $gendir/dw/core/VersionCurrent.h
+  echo -e "#ifndef DW_CORE_VERSION_EXTRA_HPP__\\n#define DW_SDK_VERSION_GIT_DESCRIPTION \"none\"\\n#define DW_SDK_BUILD_COMPILER_STRING \"GNU\"\\n#define DW_SDK_BUILD_TYPE \"Debug\"\\n#endif\\n" > $gendir/dw/core/VersionExtra.hpp
+fi
 
 # Clean up backup file left behind by sed.
 rm "${COMPDB_FILE}.bak"
